10482ce875cf689d3245729b1ab73f954ac99f34
sherry <sherry.kisilu@studebnt.moringaschool.com>
2022-08-15 12:29:54 +0300

    done

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 function shout(string) {
   return string.toUpperCase();
 }
-// logshout.string()
-// console.log(string)
-// return String.toUpperCase();
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
 function whisper(string){
   return string.toLowerCase();
 }
 function sayHiToHeadphonedRoommate(string){
-  if(string===string.toLowerCase){
+  if(string===string.toLowerCase()){
     return `I can't hear you!`
   }
-  else if(string===string.toUpperCase){
-    return 'YES INDEED'
+  else if(string===string.toUpperCase()){
+    return 'YES INDEED!'
   }  
   else (string===`Let's have dinner together`)
-    return 'I would love to'
+    return 'I would love to!'
 }
 
 console.log(shout('hello'));
 console.log(whisper('HELLO'));
 console.log(sayHiToHeadphonedRoommate('hi'));
+logShout('string');
+logWhisper('STRING');
